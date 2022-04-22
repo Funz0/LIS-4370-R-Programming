30847984a3dc88a6fc1c00760b1dc56683e3e583
Alejandro Cepeda <acepeda19@usf.edu>
2022-04-21 23:22:18 -0400

    update Week11_Cepeda.R

--- Week 11/Week11_Cepeda.R
+++ Week 11/Week11_Cepeda.R
@@ -1,19 +1,13 @@
-
 # the function below contains a bug,
 # fix it!
 tukey_multiple <- function(x) {
-  
   outliers <- array(TRUE,dim=dim(x))
-  
   for (j in 1:ncol(x)) {
     outliers[,j] <- outliers[,j] && tukey.outlier(x[,j])
   }
-  
+  outlier.vec <- vector(length=nrow(x))
   for (i in 1:nrow(x)) { 
     outlier.vec[i] <- all(outliers[i,]) 
   } 
-  
-  outlier.vec <- vector(length=nrow(x))
-  
   return(outlier.vec) 
 }
